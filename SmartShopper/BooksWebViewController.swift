db62fca00621aa11581bcfe9a9ec5aea6b03c130
Megha Mahna <meghamahna04@gmail.com>
2019-11-20 14:07:30 -0500

    Embedded web view for book category

---
+++ SmartShopper/BooksWebViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BooksWebViewController.swift
+//  SmartShopper
+//
+//  Created by Megha Mahna on 2019-11-20.
+//  Copyright © 2019 meghamahna. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class BooksWebViewController: UIViewController, WKNavigationDelegate {
+
+     var bookDelegate: BooksTableViewController?
+       var myWebView: WKWebView!
+           
+           override func loadView() {
+               
+               myWebView = WKWebView()
+               myWebView.navigationDelegate = self
+               view = myWebView
+               
+           }
+           
+           override func viewDidLoad() {
+               super.viewDidLoad()
+               
+               let url = bookDelegate?.bookData![(bookDelegate?.currentIndex)!].url
+               let url1 = URL(string: url!)!
+               let request = URLRequest(url: url1)
+               
+               myWebView.load(request)
+               myWebView.allowsBackForwardNavigationGestures = true
+           }
+
+
+}
