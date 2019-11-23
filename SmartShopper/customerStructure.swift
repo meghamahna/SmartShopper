4e2499cb21771aa172e7a3f47de77e6989177415
Manjinder kaur <manusidhu181@gmail.com>
2019-11-23 18:20:43 -0500

    definition markup added for customer struct about its functionality

--- SmartShopper/customerStructure.swift
+++ SmartShopper/customerStructure.swift
@@ -8,10 +8,18 @@
 
 import Foundation
 
+
+//MARK:- customer struct
+// this struct has three properties ,all of these are strings.
+
 struct Customer {
     var username: String
     var password: String
     var email: String
 }
 
+
+//here is an array of type of that customer struct
+//it will store the data for username,password and email.
+//this is being used to store the credientials of user,to make him able to be registered and login by using that credientials
 var customers = [Customer]()
